76dbcf052e3de0024edac2bcd406df3543e0cbf7
John Doe <johndoe@example.com>
2025-09-13 23:15:40 +0300

    [Your CHANGE]

--- src/Utilities/GetMyToken.ts
+++ src/Utilities/GetMyToken.ts
@@ -3,14 +3,20 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export default async function GetMyToken() {
-  const encryptedToken = (await cookies()).get(
-    "next-auth.session-token"
-  )?.value;
+  try {
+    const encryptedToken =
+      (await cookies()).get("next-auth.session-token")?.value ||
+      (await cookies()).get("__Securenext-auth.session-token")?.value;
 
-  const token = await decode({
-    token: encryptedToken,
-    secret: process.env.NEXTAUTH_SECRET!,
-  });
+    if (!encryptedToken) return null;
 
-  return token?.token;
+    const token = await decode({
+      token: encryptedToken,
+      secret: process.env.NEXTAUTH_SECRET!,
+    });
+
+    return token?.token;
+  } catch (err) {
+    return null;
+  }
 }
